152a39a0346a3996ab187f98140f300250a18b83
Lee Ian Ee <35843006+leeianee@users.noreply.github.com>
2018-12-31 19:08:29 +0800

    Add files via upload

---
+++ Main.ino
@@ -0,0 +1,15 @@
+void loop(){
+ tape();
+  obstacle();
+  linetrack(80);
+  claw(0,60);
+  green();
+  linetrack(80);
+  rampchk();
+  //include last room program in tape();//
+  /*Serial.print(front);
+  Serial.print(" ");
+  Serial.println(side);
+  */
+}
+
